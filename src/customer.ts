74cb2807b37d43993e88878777d911dd13d91872
tobiasruck <ruck.tobias@gmail.com>
2019-10-13 22:55:51 +0200

    Added genesis address+nuke and improved style

--- src/customer.ts
+++ src/customer.ts
@@ -2,13 +2,16 @@ import { Wallet } from 'becash'
 
 window.addEventListener('load', async () => {
     const wallet = await Wallet.generateOrLoad()
-    console.log('address', wallet.offlineGenesisAddress())
+    const genesisAddressField = document.getElementById('genesisAddress')! as HTMLInputElement
+    const nukeButton = document.getElementById('nuke')! as HTMLButtonElement
     const requestArea = document.getElementById('request')! as HTMLTextAreaElement
     const responseArea = document.getElementById('response')! as HTMLTextAreaElement
     const pay = document.getElementById('pay')! as HTMLTextAreaElement
 
     const sweepAddressField = document.getElementById('sweepAddress')! as HTMLInputElement
     const sweepButton = document.getElementById('sweep')! as HTMLButtonElement
+
+    genesisAddressField.value = wallet.offlineGenesisAddress()
 
     pay.addEventListener('click', () => {
         const response = wallet.payOfflineJson(requestArea.value)
@@ -19,5 +22,13 @@ window.addEventListener('load', async () => {
     sweepButton.addEventListener('click', async () => {
         const response = await wallet.sweepToOnline(sweepAddressField.value, 1000)
         console.log(response)
+    })
+
+    nukeButton.addEventListener('click', () => {
+        if (confirm('really nuke your wallet?')) {
+            localStorage.removeItem('be.cash/secret')
+            localStorage.removeItem('be.cash/nonce')
+            location.reload()
+        }
     })
 })
